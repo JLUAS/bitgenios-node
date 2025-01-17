5a951a5fbae2168e6127e3dad72db65bc2ff71d8
JLUAS <jlurquieta1@gmail.com>
2025-01-17 13:25:42 -0600

    configuracion de servidor correcta

--- bitgenios-node/index.js
+++ bitgenios-node/index.js
@@ -5,33 +5,30 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const port = 3000;
 
-app.use(bodyParser.json());
-
 // Configuración del transporte de correo
 const transporter = nodemailer.createTransport({
-  host: 'mail.bitgenios.com', // Asegúrate de que sea el host correcto
-  port: 587, // Usa 587 para conexiones STARTTLS
-  secure: false, // No uses SSL directamente, activa STARTTLS
+  host: 'mail.bitgenios.com',
+  port: 465, // O 25
+  secure: true, // Para SSL
   auth: {
     user: 'contacto@bitgenios.com',
-    pass: 'c0nt4c70-bg%c0m.' 
-  },
-  tls: {
-    rejectUnauthorized: false // Desactiva la validación del certificado si no es necesario
+    pass: 'c0nt4c70-bg%c0m.'
   }
 });
 
+
 // Ruta para enviar formulario de contacto
-app.post('/send-email', (req, res) => {
-  console.log("hola")
+app.post('/send-email', async (req, res) => {
+  console.log("Recibido formulario");
   const { name, email, message } = req.body;
 
   const mailOptions = {
-    from: email,
-    to: 'jlurquieta1@gmail.com',
+    from: 'contacto@bitgenios.com', // Usa siempre el remitente autenticado
+    to: 'contacto@bitgenios.com',
     subject: `Contacto desde la página web - ${name}`,
     text: `
       Nombre: ${name}
@@ -39,32 +36,27 @@ app.post('/send-email', (req, res) => {
       Información: ${message}
     `
   };
+
   const mailOptions2 = {
-    from: 'jlurquieta1@gmail.com',
+    from: 'contacto@bitgenios.com', // Usa siempre el remitente autenticado
     to: email,
     subject: 'Gracias por ponerte en contacto',
     text: `Hola ${name},\n\nGracias por ponerte en contacto con nosotros. Tu mensaje ha sido recibido correctamente. Nos pondremos en contacto contigo lo antes posible.\n\nSaludos,\nEl equipo de BITGENIOS`
   };
 
-  transporter.sendMail(mailOptions2, (error, info) => {
-    if (error) {
-      console.error('Error enviando correo de agradecimiento:', error);
-    } else {
-      console.log('Correo de agradecimiento enviado:', info.response);
-    }
-  });
-  // Envío del correo principal
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      res.status(500).send('Error enviando email');
+  try {
+    // Enviar correo de agradecimiento
+    await transporter.sendMail(mailOptions2);
+    console.log('Correo de agradecimiento enviado');
 
-    } else {
-      console.log('Email enviado:', info.response);
-      // Envío del correo de agradecimiento
-
-      res.status(200).send('Email enviado');
-    }
-  });
+    // Enviar correo principal
+    await transporter.sendMail(mailOptions);
+    console.log('Correo principal enviado');
+    res.status(200).send('Email enviado');
+  } catch (error) {
+    console.error('Error enviando correo:', error);
+    res.status(500).send('Error enviando email');
+  }
 });
 
 app.listen(port, () => {
